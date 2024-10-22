2f47b1f9db2a22748a7493cd723fef277f72d362
Andreas-Bueno <andreasdbueno@hotmail.com>
2024-10-22 14:30:26 -0300

    hash password implements

--- src/main/java/com/example/demo/service/UserService.java
+++ src/main/java/com/example/demo/service/UserService.java
@@ -2,9 +2,10 @@ package com.example.demo.service;
 
 import com.example.demo.model.User;
 import com.example.demo.repository.UserRepository;
+import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
-
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 
 @Service
 public class UserService {
@@ -12,6 +13,12 @@ public class UserService {
     @Autowired
     private UserRepository userRepository;
 
+    private final BCryptPasswordEncoder passwordEncoder;
+
+    // Inicializando o BCryptPasswordEncoder no construtor
+    public UserService() {
+        this.passwordEncoder = new BCryptPasswordEncoder();
+    }
 
     public User registerUser(User userDTO) {
 
@@ -23,7 +30,11 @@ public class UserService {
         User user = new User();
         user.setName(userDTO.getName());
         user.setEmail(userDTO.getEmail());
-        user.setPwd(userDTO.getPwd());
+
+        // Aqui você deve garantir que a senha do UserDTO está sendo usada
+        String hashedPassword = passwordEncoder.encode(userDTO.getPwd()); // Modificado para usar userDTO
+
+        user.setPwd(hashedPassword); // Define a senha hasheada no objeto user
         user.setPhone(userDTO.getPhone());
 
         return userRepository.save(user);
